48562ccb5782e47b44f57289d9b80ab2cce9a054
Kristyn Ginski <Oberst54!>
2017-03-17 13:18:01 -0400

    add new functions

--- script.js
+++ script.js
@@ -1,24 +1,88 @@
 let suits = ['❤', '⬥', '♣', '♠'];
 let names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
-// let values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14']
-
+let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
 let deck = [];
 
-suits.forEach( function(cardSuit, i) {
-  names.forEach( function(cardName, x) {
-    // values.forEach( function(cardValue, e) {
+
+var makeCards = function(suits, names, values) {
+  $.each(suits, function(i, cardSuit) {
+    $.each(values, function(e, cardVal) {
       let card = {};
       card.suit = suits[i];
-      card.name = names[x];
-      // card.value = values[e];
-
+      card.name = names[e];
+      card.value = values[e];
       deck.push(card);
-    })
-  })
-// })
+    });
+    return deck
+  });
+}
+makeCards(suits, names, values);
+console.log(deck)
 
-console.log(deck);
+
+
+
+// Prototype method
+
+// Array.prototype.shuffle = function() {
+//   let input = this;
+
+//   for (let i = input.length-1; i>=0; i--) {
+//     let randomIndex = Math.floor(Math.random() * (i+1));
+//     let itemAtIndex = input[randomIndex];
+
+//     input[randomIndex] = input[i];
+//     input[i] = itemAtIndex;
+//   }
+//   return input
+// }
+
+// deck.shuffle();
+// console.log(deck)
+
+
+
+
+
+
+
+
+
+
+// randomize method
+
+// let randomize = function(a, b) {
+//   return Math.random() - .5;
+// }
+// deck.sort(randomize);
+
+
+
+
+
+// Fishr Yates Method
+
+// let shuffleDeck = function(array) {
+//   var count = array.length, randomIndex, temp;
+//   while(count) {
+//     randomIndex = Math.random() * count-- | 0;
+//     temp = array[count];
+//     array[count] = array[randomIndex];
+//     array[randomIndex] = temp;
+//   }
+// }
+
+// shuffleDeck(deck);
+// console.log(deck);
+
+
+
+
+
+
+
+
 
 
 
